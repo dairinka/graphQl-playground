6b4b61ef8fbe60280adf184bb609108831d75804
dairinka <interest4me@gmail.com>
2023-06-06 23:27:01 +0300

    fix:  add styles for insurance

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -10,19 +10,19 @@ const NotFound: FC = (): JSX.Element => {
   };
 
   return (
-    <article className="absolute top-0 h-screen w-screen bg-dark-blue">
-      <div className="absolute top-0 z-0 h-[50vh] w-screen bg-gray"></div>
-      <div className="relative z-10 mx-auto mt-10 h-[500px] w-screen md:mt-[50vh] md:-translate-y-1/2 lg:w-[1000px] xl:h-[600px]">
-        <div className="bg-content mx-5 flex h-full w-full grow flex-col items-center bg-left bg-no-repeat text-dark-blue sm:items-end sm:bg-[url('./bg_404.png')] sm:bg-[length:500px_300px] md:bg-[length:600px_400px] lg:bg-[length:700px_450px]">
-          <div className="text-center sm:mr-20">
-            <h1 className="font-Impact text-5xl font-bold tracking-wider md:text-7xl lg:text-8xl">
+    <article className="page absolute top-0 h-screen w-screen bg-dark-blue">
+      <div className="page-bg absolute top-0 z-0 h-[50vh] w-screen bg-gray"></div>
+      <div className="page-block relative z-10 mx-auto mt-10 h-[500px] w-screen md:mt-[50vh] md:-translate-y-1/2 lg:w-[1000px] xl:h-[600px]">
+        <div className="page-block-content bg-content mx-5 flex h-full w-full grow flex-col items-center bg-left bg-no-repeat text-dark-blue sm:items-end sm:bg-[url('./bg_404.png')] sm:bg-[length:500px_300px] md:bg-[length:600px_400px] lg:bg-[length:700px_450px]">
+          <div className="page-block-header text-center sm:mr-20">
+            <h1 className="page-block-h1 font-Impact text-5xl font-bold tracking-wider md:text-7xl lg:text-8xl">
               404
             </h1>
-            <h2 className="mt-4 font-sans text-base font-bold lg:text-2xl">
+            <h2 className="page-block-h2 mt-4 font-sans text-base font-bold lg:text-2xl">
               <FormattedMessage id="PAGE_NOT_FOUND" />
             </h2>
           </div>
-          <div className="mt-10 text-center sm:mr-[4rem] lg:mr-[5.5rem] xl:mr-[7rem]">
+          <div className="page-block-btn mt-10 text-center sm:mr-[4rem] lg:mr-[5.5rem] xl:mr-[7rem]">
             <button
               className="rounded-md bg-green px-16 py-3 font-sans text-base hover:brightness-110 lg:text-2xl"
               onClick={routeChange}
